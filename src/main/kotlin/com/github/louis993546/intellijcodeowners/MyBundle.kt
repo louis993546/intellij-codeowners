88c2861f0d93938311606d331ac4c89b4e5019f6
GitHub Action <action@github.com>
2021-04-28 12:10:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/louis993546/intellijcodeowners/MyBundle.kt
+++ src/main/kotlin/com/github/louis993546/intellijcodeowners/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.louis993546.intellijcodeowners
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/louis993546/intellijcodeowners/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.louis993546.intellijcodeowners.services
+
+import com.github.louis993546.intellijcodeowners.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
